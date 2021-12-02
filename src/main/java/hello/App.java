bfeebe3320dfc790f84b52a4a49485467503dded
taixingbi <bitaihang@gmail.com>
2021-12-02 15:34:23 -0500

    first commit

---
+++ src/main/java/hello/App.java
@@ -0,0 +1,14 @@
+package hello;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("----------app start----------");
+        HttpDownload httpDownload = new HttpDownload();
+        String uri = "https://media-dev-eu-2.mirakl.net/SOURCE/021b10aacb7d48978c00d77f2a56b728";
+        String localPath = "./tmp/";
+        String localName= "test.webp";
+        httpDownload.process(uri, localPath + localName);
+        System.out.println("----------app end----------");
+    }
+}
---
+++ src/main/java/hello/HttpDownload.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class HttpDownload {
+    public void process(String uri, String localFileName) throws IOException {
+        File myFile = new File(localFileName);
+        CloseableHttpClient client = HttpClients.createDefault();
+        try (CloseableHttpResponse response = client.execute(new HttpGet(uri))) {
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                try (FileOutputStream outstream = new FileOutputStream(myFile)) {
+                    entity.writeTo(outstream);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
